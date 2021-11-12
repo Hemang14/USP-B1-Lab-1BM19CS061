7fca11ca10b4e6e10ff099ff51bccbe4dba00c68
HEMANG SINGH <hemang.cs19@bmsce.ac.in>
2021-11-12 17:50:09 +0530

    Add files via upload

---
+++ first.sh
@@ -0,0 +1,41 @@
+
+
+echo"This is my first file"
+
+
+echo"Command to be executed: "
+date
+cal
+ls -l
+pwd
+
+echo " Todays date is `date`"
+
+
+
+
+
+
+
+
+
+
+
+echo "There are `ls wc` files in `pwd`"
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
